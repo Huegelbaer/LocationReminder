82fe26e3a5f8b98bcadfe58abb3a4ff82c4f4675
Anna Steinhilber <annasteinhilber@gmx.de>
2021-12-22 16:41:58 +0100

    declare SaveReminderViewModek as viewModel

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -63,7 +63,7 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single {
+            viewModel {
                 SaveReminderViewModel(
                     appContext,
                     get() as ReminderDataSource
